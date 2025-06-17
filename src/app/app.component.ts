e95612927ffd0fa87df320d5ac29a82761cdd9ab
Amol Shinde <amolshinde8976@gmail.com>
2025-06-17 21:42:10 +0530

    InterPloation In Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tut';
+  title = 'Angular App';
+  name = 'Amol Shinde';
+  city = 'Mumbai';
+  x = 10;
+  y = 20;
 }
